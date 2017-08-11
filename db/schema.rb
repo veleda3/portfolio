28f9262e2cd62447454a419292841753526b0314
Daniel Velez <velez.a.daniel@gmail.com>
2017-08-11 19:04:45 -0400

    blog added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810230509) do
+ActiveRecord::Schema.define(version: 20170811221558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 20170810230509) do
     t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.text "description"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "banner_image_url"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
   create_table "questions", force: :cascade do |t|
