2b9ab87da4d7c8e7ab4128d7d39a81e9d2634d6a
Daniel Velez <velez.a.daniel@gmail.com>
2017-08-12 12:10:39 -0400

    changes to application.rb to add pics to heroku

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.serve_static_assets = true
 
 module Portfolio
   class Application < Rails::Application
