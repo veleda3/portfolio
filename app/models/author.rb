1131e83d839865f1f5f0de385085ca8bd45732bc
Daniel Velez <velez.a.daniel@gmail.com>
2017-10-03 23:15:01 -0400

    got rid off of additional login

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,7 @@
 class Author < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
   validates :email, :password, :password_confirmation, :name, presence: true
