17295089f2904ec04e96a8f94d26fbbffd3469ce
Daniel Velez <velez.a.daniel@gmail.com>
2017-10-22 22:44:43 -0400

    still having issues with landing.css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,16 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( animate.css )
+Rails.application.config.assets.precompile += %w( home.css )
+Rails.application.config.assets.precompile += %w( linear-icons.css )
+Rails.application.config.assets.precompile += %w( owl.carousel.css )
+Rails.application.config.assets.precompile += %w( owl.theme.css )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( landing.scss )
+
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( owl.carousel.min.js )
 Rails.application.config.assets.precompile += %w( scrolling-nav.js )
@@ -13,15 +23,6 @@ Rails.application.config.assets.precompile += %w( map.js )
 Rails.application.config.assets.precompile += %w( validator.min.js )
 Rails.application.config.assets.precompile += %w( tether.min.js )
 
-Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( animate.css )
-Rails.application.config.assets.precompile += %w( home.css )
-Rails.application.config.assets.precompile += %w( linear-icons.css )
-Rails.application.config.assets.precompile += %w( owl.carousel.css )
-Rails.application.config.assets.precompile += %w( owl.theme.css )
-Rails.application.config.assets.precompile += %w( style.css )
-Rails.application.config.assets.precompile += %w( font-awesome.min.css )
-Rails.application.config.assets.precompile += %w( landing.scss )
 
 
 # Add additional assets to the asset load path.
